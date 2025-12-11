cfb53e093a93788f62caee151472629d089654ec
n_syromyatnikov <112897508+Syrnik1307@users.noreply.github.com>
2025-12-11 19:28:59 +0300

    chore: bump build stamp for cache bust

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const BUILD_STAMP = '2025-12-06T00:30Z';
+const BUILD_STAMP = '2025-12-06T01:05Z';
 if (typeof console !== 'undefined') {
   console.info('Frontend build', BUILD_STAMP);
 }
